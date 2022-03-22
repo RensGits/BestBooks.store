713c4f071ac3d396e535d2f78c2f5db74c814c12
Rens van Driel <rens.van.a.driel@sogeti.com>
2022-03-22 10:59:27 +0100

    Searchresults now load instantly

--- src/components/navigation/SearchBar.js
+++ src/components/navigation/SearchBar.js
@@ -2,9 +2,9 @@ import './SearchBar.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { updateInput } from '../../redux/slices/searchInputSlice'
 import { useEffect, useState } from 'react'
-import _ from 'lodash'
 import { BiSearch } from 'react-icons/bi'
 import SearchResult from './SearchResult'
+import _ from 'lodash'
 
 
 export default function SearchBar(){
@@ -18,19 +18,30 @@ export default function SearchBar(){
     const currentSearchInput = useSelector((state) => state.searchInput)
     useEffect(() => {
         const filteredBooks = []
-       _.forEach(allBooks, function(value2,index){
+       _.forEach(allBooks, function(value){ // maps over all books
 
-        const formatedObject = _.mapValues(value2, _.method('toLowerCase'));
-           if(_.includes(formatedObject, currentSearchInput.searchInput.toLowerCase())){
-                filteredBooks.push(value2)
-           }
+        const formatedObject = _.mapValues(value, _.method('toLowerCase')); // sets all values in book objects to lowercase
+        if(currentSearchInput.searchInput.length > 0){
+           if(_.includes(formatedObject.title, currentSearchInput.searchInput.toLowerCase())){ // pushes book to temporary array if searchinput matches title
+                filteredBooks.push(value)
+                }
+           if(_.includes(formatedObject.author, currentSearchInput.searchInput.toLowerCase())){ // pushes book to temporary array if searchinput matches author
+            filteredBooks.push(value)   
+                }
+            if(_.includes(formatedObject.primary_isbn10, currentSearchInput.searchInput.toLowerCase())){ // pushes book to temporary array if searchinput matches isbn10
+                filteredBooks.push(value)
+                }
+            if(_.includes(formatedObject.primary_isbn13, currentSearchInput.searchInput.toLowerCase())){ // pushes book to temporary array if searchinput matches isbn13
+                filteredBooks.push(value)
+                }
+            }
        })
-       setData(filteredBooks)   
+       setData(_.uniq(filteredBooks)) // sets temporary array to local state & filters duplicates
     },[currentSearchInput, allBooks])
 
     
     
-   
+  
        
    
   
@@ -42,10 +53,10 @@ export default function SearchBar(){
             <BiSearch className='search-icon' color='light-gray' />
             {data.length !== allBooks.length &&
                 <div className='search-results-container'>
-                    {data.length === 0 &&
+                    {data.length === 0 &&   // returns no results found if filteredBooks state object is empty
                         <p className='no-results-text'>No results found</p>
                     }
-                    {data.length !== allBooks.length &&
+                    {data.length !== allBooks.length && // returns searchresult for each object in filteredBooks state object
                         data.map((book)=>{
                             return <SearchResult data={book}  />
                         })
